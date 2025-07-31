9ee0653ad9206a0d57d6f52a30c8b6284761aaaf
kauapugliesi <pugliesikaua@gmail.com>
2025-07-31 13:15:20 -0300

    Chess Exception and Chess Position

---
+++ src/Chess/ChessException.java
@@ -0,0 +1,9 @@
+package Chess;
+
+public class ChessException extends RuntimeException{
+    private static final long SerialVersionUID = 1L;
+
+    public ChessException(String msg){
+        super(msg);
+    }
+}
